04fdd2687dc69556123481d5bfaa3a97743e5cf7
KomoBIG <cubeque@wp.pl>
2023-10-11 19:07:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/dotslicer/MainMenu.java
@@ -0,0 +1,30 @@
+package com.example.dotslicer;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MainMenu extends Fragment {
+
+    public MainMenu() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.main_menu, container, false);
+    }
+
+}
