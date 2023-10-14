8f1dac5b9af95760bbf83bd43967e8311c4722c4
KomoBIG <cubeque@wp.pl>
2023-10-14 13:29:56 +0200

    Basic menu and navigation

---
+++ app/src/main/java/com/example/dotslicer/GameActivity.java
@@ -0,0 +1,25 @@
+package com.example.dotslicer;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode==KeyEvent.KEYCODE_BACK) {
+            return false;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
