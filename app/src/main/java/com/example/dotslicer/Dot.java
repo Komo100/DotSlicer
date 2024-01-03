ba4bb5e7356b918d4b6a6929b8e6d7fb6bcc0864
Komo <97801971+Komo100@users.noreply.github.com>
2024-01-03 18:43:21 +0100

    Additional gameplay

--- app/src/main/java/com/example/dotslicer/Dot.java
+++ app/src/main/java/com/example/dotslicer/Dot.java
@@ -1,38 +1,50 @@
 package com.example.dotslicer;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.util.DisplayMetrics;
-import android.view.LayoutInflater;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import androidx.core.widget.TextViewCompat;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.core.content.ContextCompat;
 
-public class Dot extends LinearLayout {
+import java.util.Observable;
 
-    private float dX, dY;
-    private float centerX, centerY;
+public class Dot extends AppCompatTextView {
+
+    private float dX, dY, ClickX, ClickY, CenterX, CenterY;
+    private boolean isClick;
     Integer number;
     private int base_radius = 10;
     private int radius;
+    private boolean Clicked;
 
     public Dot(Context context, float X, float Y, int number) {
         super(context);
-        this.setX(X);
-        this.setY(Y);
-        this.number = number;
         radius = (int) (base_radius*number + 50);
-        inflate(getContext(), R.layout.dot_layout, this);
-        TextView numberTextView = (TextView) findViewById(R.id.numberTextView);
-        numberTextView.setText(Integer.toString(this.number));
-        LinearLayout layout = (LinearLayout) findViewById(R.id.dot_layout);
-        layout.getLayoutParams().height = 2*radius;
-        layout.getLayoutParams().width = 2*radius;
+        this.CenterX = X;
+        this.CenterY = Y;
+        this.setX(X - radius);
+        this.setY(Y - radius);
+        this.number = number;
+        this.Clicked = false;
+
+        radius = (int) (base_radius*number + 50);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(2*radius, 2*radius);
+        this.setLayoutParams(layoutParams);
+        this.setBackground(ContextCompat.getDrawable(context, R.drawable.dot));
+        //inflate(getContext(), R.layout.dot_layout, this);
+        this.setText(Integer.toString(this.number));
+        this.setTextSize(20);
+        this.setGravity(Gravity.CENTER);
+        //LinearLayout layout = (LinearLayout) findViewById(R.id.dot_layout);
+        //layout.getLayoutParams().height = 2*radius;
+        //layout.getLayoutParams().width = 2*radius;
     }
     @Override
     public boolean onTouchEvent(MotionEvent e) {
@@ -42,25 +54,45 @@ public class Dot extends LinearLayout {
 
                 dX = getX() - e.getRawX();
                 dY = getY() - e.getRawY();
+                ClickX = e.getX();
+                ClickY = e.getY();
+                isClick = true;
                 break;
-
+            case MotionEvent.ACTION_UP:
+                if(isClick) {
+                    Clicked = true;
+                }
+                break;
             case MotionEvent.ACTION_MOVE:
-
-                this.animate()
-                        .x(e.getRawX() + dX)
-                        .y(e.getRawY() + dY)
-                        .setDuration(0)
-                        .start();
-                this.setX(e.getRawX() + dX);
-                this.setY(e.getRawY() + dY);
+                if(true) {
+                    this.animate()
+                            .x(e.getRawX() + dX)
+                            .y(e.getRawY() + dY)
+                            .setDuration(0)
+                            .start();
+                    this.setX(e.getRawX() + dX);
+                    this.setY(e.getRawY() + dY);
+                    isClick = false;
+                }
                 break;
-
             default:
                 return false;
         }
         return true;
     }
 
+    public float getCenterX() {
+        return this.getX() + radius;
+    }
+
+    public float getCenterY() {
+        return this.getY() + radius;
+    }
+
+    public Integer getNumber() { return number; }
+    public boolean isClicked() {
+        return Clicked;
+    }
     public int getRadius() {
         return radius;
     }
--- app/src/main/java/com/example/dotslicer/GameActivity.java
+++ app/src/main/java/com/example/dotslicer/GameActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.Color;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.transition.Fade;
 import android.util.DisplayMetrics;
@@ -21,16 +22,17 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.google.android.material.color.MaterialColors;
 
+import java.util.Observer;
 import java.util.Random;
 import java.util.Vector;
 
 
 public class GameActivity extends AppCompatActivity {
 
-    private Vector<Dot> vector_of_dots;
+    private Vector<Dot> VectorOfDots;
     private Integer number_to_get = 20;
 
-    //float dX, dY;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +41,7 @@ public class GameActivity extends AppCompatActivity {
         getWindow().setExitTransition(new Fade());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        vector_of_dots = GenerateDots();
+        VectorOfDots = GenerateDots();
         findViewById(R.id.button_exit_game).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -48,9 +50,9 @@ public class GameActivity extends AppCompatActivity {
                 finish();
             }
         });
+
         final TextView number_to_get_tv = (TextView) findViewById(R.id.number_to_get);
-        number_to_get_tv.setText(
-                "Number to get: " + number_to_get.toString());
+        number_to_get_tv.setText("Number to get: " + number_to_get.toString());
     }
 
     private Vector<Dot> GenerateDots() {
@@ -66,19 +68,19 @@ public class GameActivity extends AppCompatActivity {
             float pos_x;
             float pos_y;
             int number;
-
+            Dot dot;
             do {
                 Random random = new Random();
                 number = random.nextInt(9) + 1;
-                pos_y = (float)random.nextInt(height - 400) + 100;
-                pos_x = (float)random.nextInt(width - 400) + 100;
-            } while(DotTooClose(vector_of_dots, pos_x, pos_y));
+                pos_y = (float)random.nextInt(height);
+                pos_x = (float)random.nextInt(width);
+                dot = new Dot(getApplicationContext(), pos_x, pos_y, number);
+            } while(DotTooClose(vector_of_dots, dot));
 
-            Dot dot = new Dot(getApplicationContext(), pos_x, pos_y, number);
             layout.addView(dot);
+            CreateDotListener(dot, layout);
             vector_of_dots.add(dot);
         }
-
         return vector_of_dots;
     }
 
@@ -86,16 +88,72 @@ public class GameActivity extends AppCompatActivity {
         return (float)Math.sqrt((x0 - x1)*(x0 - x1) + (y0 - y1)*(y0 - y1));
     };
 
-    private boolean DotTooClose(Vector<Dot> vector, float x0, float y0) {
+    private boolean DotTooClose(Vector<Dot> vector, Dot dot) {
         for(int i = 0; i < vector.size(); i++) {
-
-            float x1 = vector.get(i).getX();
-            float y1 = vector.get(i).getY();
-            float radius = vector.get(i).getRadius();
-
-            if(distance(x0, y0, x1, y1) <= radius*2) return true;
+            if(CollidedWith(dot, vector) != null || TooCloseToEdge(dot)) return true;
         }
         return false;
+    }
+    private Dot CollidedWith(Dot dot, Vector<Dot> vector_of_dots) {
+        for (Integer i = 0; i < vector_of_dots.size(); i++) {
+            Dot dot1 = vector_of_dots.get(i);
+            float dotCenterX = dot.getX() + dot.getWidth()/2;
+            float dotCenterY = dot.getY() + dot.getHeight()/2;
+            float dot1CenterX = dot1.getX() + dot1.getWidth()/2;
+            float dot1CenterY = dot1.getY() + dot1.getHeight()/2;
+            boolean isIntersecting = (distance(dotCenterX, dotCenterY, dot1CenterX, dot1CenterY) < dot.getRadius() + dot1.getRadius());
+            if((vector_of_dots.get(i) != dot && isIntersecting) ) {
+                return vector_of_dots.get(i);
+            };
+
+        }
+        return null;
+    }
+
+    private boolean TooCloseToEdge(Dot dot) {
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+        int height = metrics.heightPixels;
+        int width = metrics.widthPixels;
+        return (dot.getX() < width*0.2 ||
+                dot.getY() < height*0.2 ||
+                dot.getX() + dot.getWidth() > width*0.8 ||
+                dot.getY() + dot.getHeight() > height*0.8);
+    }
+
+    private void CreateDotListener(Dot dot, ConstraintLayout layout) {
+        dot.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent e) {
+                boolean ReturnedValue = dot.onTouchEvent(e);
+                if((e.getAction() == MotionEvent.ACTION_UP)) {
+                    if (dot.isClicked()) {
+                        layout.removeView(dot);
+                        VectorOfDots.remove(dot);
+                    } else {
+                        int num1 = dot.getNumber();
+                        Dot CollidedWithThis = CollidedWith(dot, VectorOfDots);
+                        if(CollidedWithThis != null) {
+                            int num2 = CollidedWithThis.getNumber();
+                            int new_number = num1 + num2;
+                            float new_posx = dot.getCenterX();
+                            float new_posy = dot.getCenterY();
+                            layout.removeView(dot);
+                            layout.removeView(CollidedWithThis);
+                            VectorOfDots.remove(dot);
+                            VectorOfDots.remove(CollidedWithThis);
+                            Dot new_dot = new Dot(getApplicationContext(), new_posx, new_posy, new_number);
+                            layout.addView(new_dot);
+                            CreateDotListener(new_dot, layout);
+                            VectorOfDots.add(new_dot);
+
+                        }
+                    }
+                }
+                return ReturnedValue;
+            }
+        });
     }
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
@@ -104,5 +162,6 @@ public class GameActivity extends AppCompatActivity {
         }
         return super.onKeyDown(keyCode, event);
     }
+
 
 }
